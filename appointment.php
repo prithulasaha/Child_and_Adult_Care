b1914bb31293003d5458ab30cb64ae127b91331e
Prithula Saha <sahaprithula@gmail.com>
2025-08-16 00:17:48 +0600

    Add files via upload

---
+++ appointment.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>General Notice</title>
+    <link rel="stylesheet" href="notice.css">
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">KID<span>2</span>ELDERS</a>
+        <nav class="navbar">
+          <a href="home.php#home">home</a>
+          <a href="home.php#about">about</a>
+          <a href="home.php#gallery">gallery</a>
+          <a href="home.php#batch">batch</a>
+          <a href="counselling.php">counselling</a>
+          <div class="dropdown">
+              <button class="dropbtn">home services
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                
+                <a href="nanny.php">nanny</a>
+                <a href="nurse.php">nurse</a>
+              </div>
+          </div>
+          <div class="dropdown">
+              <button class="dropbtn">Course
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                
+                <a href="child.php">for children</a>
+                <a href="adultc.php">for adults</a>
+              </div>
+          </div>
+          <a href="recruitment.php">recruitment</a>
+          <a href="payment.php">monthly fee</a>
+          <a href="home.php#contact">contact</a>
+          <div class="dropdown">
+              <button class="dropbtn">Notice Board
+                <i class="fa fa-caret-down"></i>
+              </button>
+              <div class="dropdown-content">
+                
+                <a href="general.php">general notice</a>
+                <a href="appointment.php">appointment</a>
+                <a href="program.php">Recent Program Plan</a>
+              </div>
+          </div>
+          
+      </nav>
+    </header>
+
+    <section class="general" id="general">
+        <h1 class="heading">APPOINTMENT NOTICES</h1>
+        <div class="table-box">
+          <div class="table-row table-head">
+            <div class="table-cell">
+              <p>Notice</p>
+            </div>
+            <div class="table-cell">
+              <p>Publish Date</p>
+            </div>
+            
+          </div>
+          <div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Newly Recruited Teachers can Join from 25 December, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>14 December, 2023</p>
+            </div>
+            
+          </div>
+          <div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Newly Recruited Nannies can Join from 28 December, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>10 December, 2023</p>
+            </div>
+          </div><div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Newly Recruited Nurses can Join from 26 December, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>10 December, 2023</p>
+            </div>
+          </div><div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Interview of the Interested People Applied in Online for the post of teacher will Held On 20 November, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>12 November, 2023</p>
+            </div>
+          </div><div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Interview of the Interested People Applied in Online for the Post of Nurse will Held On 23 November, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>12 November, 2023</p>
+            </div>
+          </div><div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Interview of the Interested People Applied in Online for the Post of Nanny will Held On 26 November, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>12 November, 2023</p>
+            </div>
+          </div>
+          <div class="table-row">
+            <div class="table-cell first-cell">
+              <p>Online Job Application for the Post of Teacher, Nurse and Nanny will be taken from 25 October, 2023 to 9 November, 2023</p>
+            </div>
+            <div class="table-cell">
+              <p>12 October, 2023</p>
+            </div>
+          </div>
+        </div>
+
+    </section>
+
+</body>
+</html>
